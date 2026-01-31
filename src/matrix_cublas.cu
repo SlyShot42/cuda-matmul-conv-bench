8c639febf4715c80758692c5a47ddaeffbfbc213
Viraj Bansal <virajmohit7@gmail.com>
2026-01-30 16:21:01 -0800

    added verbosity to matrix_cublas

--- src/matrix_cublas.cu
+++ src/matrix_cublas.cu
@@ -8,13 +8,13 @@ int main(int argc, char **argv) {
     int N = (argc > 1) ? atoi(argv[1]) : 1024; // allow matrix size as input
     size_t size = N * N * sizeof(float);
 
-    float *h_A = (float *)malloc(size);
-    float *h_B = (float *)malloc(size);
-    float *h_C = (float *)malloc(size);
+    float *A = (float *)malloc(size);
+    float *B = (float *)malloc(size);
+    float *C = (float *)malloc(size);
 
     for (int i = 0; i < N * N; i++) {
-        h_A[i] = (float)(rand() % 100) / 100.0f;
-        h_B[i] = (float)(rand() % 100) / 100.0f;
+        A[i] = (float)(rand() % 100) / 100.0f;
+        B[i] = (float)(rand() % 100) / 100.0f;
     }
 
     float *d_A, *d_B, *d_C;
@@ -22,8 +22,8 @@ int main(int argc, char **argv) {
     cudaMalloc((void **)&d_B, size);
     cudaMalloc((void **)&d_C, size);
 
-    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
 
     float alpha = 1.0f;
     float beta = 0.0f;
@@ -42,13 +42,17 @@ int main(int argc, char **argv) {
     double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
     printf("GPU execution time with cuBLAS (N=%d): %f seconds\n", N, elapsed);
 
-    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+
+    if (argc > 2 && strcmp(argv[2], "verbose") == 0) {
+        printf("C[0]=%f C[mid]=%f C[last]=%f\n", C[0], C[(N/2)*N + (N/2)], C[N*N-1]);
+    }
 
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
-    free(h_A);
-    free(h_B);
-    free(h_C);
+    free(A);
+    free(B);
+    free(C);
     return 0;
 }
